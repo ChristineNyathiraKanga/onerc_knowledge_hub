72e4501e81adba3fc0e580070cd19c6b74db042a
Nigel <nigelnathan2@gmail.com>
2026-05-11 15:46:20 +0300

    feat: Add Knowledge Hub doctype with validation and integration tests

---
+++ onerc_knowledge_hub/onerc_knowledge_hub/doctype/knowledge_hub/knowledge_hub.py
@@ -0,0 +1,19 @@
+# Copyright (c) 2026, Kenya Red Cross Society and contributors
+# For license information, please see license.txt
+
+import frappe
+from frappe.model.document import Document
+from frappe.utils import today
+
+
+class KnowledgeHub(Document):
+
+	def validate(self):
+		if not self.file_attachment and not self.external_url:
+			frappe.throw("Either a file attachment or an external URL must be provided.")
+		if self.resource_category == "Tools & Templates" and not self.tools_subcategory:
+			frappe.throw("Tools & Templates resources require a subcategory.")
+
+	def before_save(self):
+		if self.status == "Published" and not self.published_date:
+			self.published_date = today()
